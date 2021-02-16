abfb8d56f238a0386b40d48831d9fadf23e036d6
kellyqjiang <45496671+kellyqjiang@users.noreply.github.com>
2021-02-16 16:06:50 -0500

    Update SongLibController.java

--- SongLib/src/view/SongLibController.java
+++ SongLib/src/view/SongLibController.java
@@ -1,3 +1,4 @@
+  
 package view;
 
 import javafx.collections.FXCollections;
@@ -64,12 +65,7 @@ public class SongLibController {
 		// select the first item
 		songs.getSelectionModel().selectFirst();
 		
-		songs
-		.getSelectionModel()
-		.selectedIndexProperty()
-		.addListener(
-				(obs, oldVal, newVal) -> 
-				showDetails()); //instead of showItemInputDialog, show details
+		songs.getSelectionModel().selectedIndexProperty().addListener((obs, oldVal, newVal) -> showDetails());
 	}
 
 	private void showDetails() {                
@@ -83,15 +79,27 @@ public class SongLibController {
 	
 	
 	public void addSong(ActionEvent e) {
-		//check if name and artist already exist
-		//popup confirm?
-		
 		//get song details from user
 		Song newSong = new Song();
 		newSong.setName(name.getText());
 		newSong.setArtist(artist.getText());
 		newSong.setAlbum(album.getText());
 		newSong.setYear(year.getText());
+		
+		boolean isCopy = false; //if we find a copy, this will turn true
+		
+		for(int i = 0; i < obvSongs.size(); i++) { //loops into obvSongs to check if the song exists or not
+			if(obvSongs.get(i).getName().equals(name.getText()) && obvSongs.get(i).getArtist().equals(artist.getText())) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("Add Error");
+				alert.setHeaderText(null);
+				alert.setContentText("A song with this name and artist already exists!");
+				alert.showAndWait();
+				isCopy = true;
+			} else {
+				break;
+			}
+		}
 		
 		//checking if at least song name and artist are filled out
 		if (name.getText().equals("") || artist.getText().equals("")) {  
@@ -100,18 +108,27 @@ public class SongLibController {
 			alert.setHeaderText(null);
 			alert.setContentText("Please add at least a song name and artist");
 			alert.showAndWait();
-		}else {
-			obvSongs.add(newSong);
+		}else if(!isCopy){ //if it's a copy, we will not go into this if statement, if it's a new song, we confirm with user
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Add Confirmation");
+			alert.setHeaderText(null);
+			alert.setContentText("Are you sure you want to add this song?");
+			Optional<ButtonType> result = alert.showAndWait();
+	        if (result.get() == ButtonType.OK) {
+	        	obvSongs.add(newSong);
+	        	
+	        	//sorting alphabetically
+	    		Comparator<Song> comparator = Comparator.comparing(Song::getName);
+	    		obvSongs.sort(comparator);
+	    		
+	    		int selectIndex = obvSongs.indexOf(newSong);
+	    		songs.getSelectionModel().select(selectIndex);
+	        }
 		}
 		
-		//sorting alphabetically
-		Comparator<Song> comparator = Comparator.comparing(Song::getName);
-		obvSongs.sort(comparator);
 	}
 	
 	public void delete(ActionEvent e) {
-		//popup confirm
-		 
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Delete Confirmation");
 		alert.setHeaderText(null);
@@ -119,12 +136,27 @@ public class SongLibController {
 		Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
         	Song song = songs.getSelectionModel().getSelectedItem();
+        	int selectIndex = obvSongs.indexOf(song);
     		obvSongs.remove(song);
+    		
+    		//select next, prev, or none depending on list after deleting the song
+    		if(obvSongs.isEmpty()) {
+    			name.setText("");
+    			artist.setText("");
+    			album.setText("");
+    			year.setText("");
+    		} else if(obvSongs.get(selectIndex) != null) {
+    			songs.getSelectionModel().select(selectIndex);
+    		} else if(obvSongs.get(selectIndex-1) != null) {
+    			songs.getSelectionModel().select(selectIndex-1);
+    		}
+    		
         }
 	}
 	
 	public void edit(ActionEvent e) {
-		//change listview automatically
+		
+		boolean isCopy = false;
 		
 		Alert alert = new Alert(AlertType.CONFIRMATION);
 		alert.setTitle("Edit Confirmation");
@@ -132,12 +164,31 @@ public class SongLibController {
 		alert.setContentText("Are you sure you want to edit this song?");
 		Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK) {
-        	Song existingSong = songs.getSelectionModel().getSelectedItem();
-    		
-    		existingSong.setName(name.getText());
-    		existingSong.setArtist(artist.getText());
-    		existingSong.setAlbum(album.getText());
-    		existingSong.setYear(year.getText());
+        	for(int i = 0; i < obvSongs.size(); i++) { //loops into obvSongs to check if the song exists or not
+    			if(obvSongs.get(i).getName().equals(name.getText()) && obvSongs.get(i).getArtist().equals(artist.getText())) {
+    				Alert alert2 = new Alert(AlertType.ERROR);
+    				alert2.setTitle("Edit Error");
+    				alert2.setHeaderText(null);
+    				alert2.setContentText("A song with this name and artist already exists!");
+    				alert2.showAndWait();
+    				isCopy = true;
+    			} else {
+    				break;
+    			}
+    		}
+        	
+        	if(!isCopy) {
+        		Song existingSong = songs.getSelectionModel().getSelectedItem();
+        		
+        		existingSong.setName(name.getText());
+        		existingSong.setArtist(artist.getText());
+        		existingSong.setAlbum(album.getText());
+        		existingSong.setYear(year.getText());
+        		
+        		//edits listview song to match once edited
+        		int selectedSong = obvSongs.indexOf(existingSong);
+        		songs.getItems().set(selectedSong, existingSong);
+        	}
         }
 	}
 }
